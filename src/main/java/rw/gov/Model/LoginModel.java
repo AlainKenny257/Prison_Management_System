2b16a61b1574de442ba17aac9a300f34c38b2202
Ishimwe Alain Kenny <alainkennyishimwe@gmail.com>
2024-04-04 20:01:02 +0200

    First Upload

---
+++ src/main/java/rw/gov/Model/LoginModel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rw.gov.Model;
+
+/**
+ *
+ * @author The_Great
+ */
+public class LoginModel {
+    private String Username;
+    private String Password;
+    
+    
+}
---
+++ src/main/java/rw/gov/PMS/PrisonManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package rw.gov.PMS;
+
+/**
+ *
+ * @author The_Great
+ */
+public class PrisonManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
